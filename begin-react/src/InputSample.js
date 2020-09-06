2cf98c03329da62a5cf32eab98f13babf6500a10
river <river9028@gmail.com>
2020-09-06 22:26:12 +0900

    :zap: 10. useRef 로 특정 DOM 선택하기 :zap: - 리액트에서 특정 DOM 을 선택해야 하는 상황에 ref를 사용 - 함수형 컴포넌트에서 ref 를 사용 할 때에는 useRef 라는 Hook 함수 사용 - 클래스형 컴포넌트에서는 콜백 함수/React.createRef 함수 사용

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
@@ -7,9 +7,11 @@ function InputSample() {
   });
 
   const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+  const nameInput = useRef();
 
   const onChange = (e) => {
     const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+
     setInputs({
       ...inputs, // 기존의 input 객체를 복사한 뒤
       [name]: value, // name 키를 가진 값을 value 로 설정
@@ -21,11 +23,18 @@ function InputSample() {
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
