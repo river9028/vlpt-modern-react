2c95a2721078135047233e7439ab212d4fc261ae
river <river9028@gmail.com>
2020-09-06 19:40:11 +0900

    :zap: 4. JSX의 기본 규칙 :zap: - 2. 작업환경 준비 - 3. 나의 첫번째 리액트 컴포넌트 - 꼭 닫혀야 하는 태그 - 꼭 감싸져야하는 태그 - JSX 안에 자바스크립트 값을 사용할 때 {} 으로 감싸기 - style 과 className - JSX 내부의 주석은 {/* 이런 형태로 */} 작성

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24, // 기본 단위 px
+    padding: "1rem", // 다른 단위 사용 시 문자열로 설정
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 주석은 화면에 보이지 않습니다 */}
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
