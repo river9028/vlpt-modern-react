3f6ab1c66056894b5636a0d56cefe906ff877868
river <river9028@gmail.com>
2020-09-07 02:25:57 +0900

    :zap: 16. useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기 :zap:
    - useEffect 로 컴포넌트가 마운트 됐을 때 (처음 나타났을 때), 언마운트 됐을 때 (사라질 때), 그리고 업데이트 될 때 (특정 props가 바뀔 때) 특정 작업을 처리
    - useEffect 첫번째 파라미터에는 함수, 두번째 파라미터에는 의존값이 들어있는 배열 (deps)
    - deps 배열을 비우게 된다면, 컴포넌트가 처음 나타날때에만 useEffect 에 등록한 함수가 호출
    - cleanup 함수: useEffect 에서는 반환 하는 함수, useEffect 에 대한 뒷정리를 해준다고 이해, deps 가 비어있는 경우에는 컴포넌트가 사라질 때 cleanup 함수가 호출
    
    - 마운트 시에 하는 작업들은 다음과 같은 사항
     props 로 받은 값을 컴포넌트의 로컬 상태로 설정
     외부 API 요청 (REST API 등)
     라이브러리 사용 (D3, Video.js 등...)
     setInterval 을 통한 반복작업 혹은 setTimeout 을 통한 작업 예약
    
    - 언마운트 시에 하는 작업들은 다음과 같은 사항
     setInterval, setTimeout 을 사용하여 등록한 작업들 clear 하기 (clearInterval, clearTimeout)
     라이브러리 인스턴스 제거
    
    - deps 에 특정 값 넣기
    - deps 에 특정 값을 넣게 된다면, 컴포넌트가 처음 마운트 될 때에도 호출이 되고, 지정한 값이 바뀔 때에도 호출
    - deps 안에 특정 값이 있다면 언마운트시에도 호출이되고, 값이 바뀌기 직전에도 호출이 됩니다
    
    -useEffect 안에서 사용하는 상태나, props 가 있다면, useEffect 의 deps 에 넣어주는게 일종의 규칙
    
    - 만약 useEffect 안에서 사용하는 상태나 props 를 deps 에 넣지 않게 된다면 useEffect 에 등록한 함수가 실행 될 때 최신 props / 상태를 가르키지 않게 됩니다.
    
    - deps 파라미터를 생략하기
    - deps 파라미터를 생략한다면, 컴포넌트가 리렌더링 될 때마다 호출

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  // deps 에 특정 값 넣기
+  useEffect(() => {
+    // console.log("컴포넌트가 화면에 나타남");
+    console.log("user 값이 설정됨");
+    console.log(user);
+    return () => {
+      // console.log("컴포넌트가 화면에서 사라짐");
+      console.log("user 가 바뀌기 전..");
+      console.log(user);
+    };
+  }, [user]);
+
+  // deps 파라미터를 생략하기
+  // useEffect(() => {
+  //   console.log(user);
+  // });
+
   return (
     <div>
       <b
