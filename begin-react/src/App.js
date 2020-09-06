0f5b8586c7f3c6383b88af43cb58c1ef4023a9ff
river <river9028@gmail.com>
2020-09-06 19:59:15 +0900

    :zap: 5. props 를 통해 컴포넌트에게 값 전달하기 :zap: - 어떠한 값을 컴포넌트에게 전달해줘야 할 때, props를 사용 - 여러개의 props, 비구조화 할당 문법을 사용하면 조금 더 코드를 간결하게 작성 가능 - 컴포넌트에 props 를 지정하지 않았을 때, defaultProps로 기본값 설정 가능 - 컴포넌트 태그 사이에 넣은 값을 조회하고 싶을 땐, props.children을 조회

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import Hello from "./Hello";
-import "./App.css";
+import Wrapper from "./Wrapper";
 
 function App() {
-  const name = "react";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 24, // 기본 단위 px
-    padding: "1rem", // 다른 단위 사용 시 문자열로 설정
-  };
-
   return (
-    <>
-      {/* 주석은 화면에 보이지 않습니다 */}
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
