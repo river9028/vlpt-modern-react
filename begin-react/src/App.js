f8a4ccd0f700ebb6ba5c2f5585eeedc8af52d73e
river <river9028@gmail.com>
2020-09-07 22:58:48 +0900

    :zap: 22. Context API 를 사용한 전역 값 관리 :zap:
    - Context API: 프로젝트 안에서 전역적으로 사용 할 수 있는 값을 관리(상태, 함수, 외부 라이브러리 인스턴스, DOM, ...)
    - Context 생성: React.createContext() 함수 사용, 파라미터 Context 의 기본값을 설정 / Provider 라는 컴포넌트 value 설정
    - Provider 에 의하여 감싸진 컴포넌트 중 어디서든지 우리가 Context 의 값을 다른 곳에서 바로 조회해서 사용

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -63,6 +63,8 @@ const reducer = (state, action) => {
   }
 };
 
+export const UserDispatch = React.createContext(null);
+
 function App() {
   const [{ username, email }, onChange, reset] = useInputs({
     username: "",
@@ -88,33 +90,19 @@ function App() {
     nextId.current += 1;
   }, [username, email, reset]);
 
-  const onToggle = useCallback((id) => {
-    dispatch({
-      type: "TOGGLE_USER",
-      id,
-    });
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    dispatch({
-      type: "REMOVE_USER",
-      id,
-    });
-  }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <>
+    <UserDispatch.Provider value={dispatch}>
       <CreateUser
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <UserList users={users} />
       <div>활성사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
 
