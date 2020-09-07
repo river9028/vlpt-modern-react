9ccd6f71b3fbf4b6f8e7929f3ed906343f613838
river <river9028@gmail.com>
2020-09-08 03:01:58 +0900

    :zap: 24. 클래스형 컴포넌트 :zap:
    - render()
    - props 를 조회 해야 할 때에는 this.props 를 조회
    - defaultProps / static 키워드와 함께 선언
    - 메서드 (보통 이름을 handle... 이라고 이름을 선언)
    - this 는 컴포넌트 인스턴스를 가르키는 방법:
     1. constructor에서 bind 작업
     2. 화살표 함수 문법을 사용
     3. 새로운 함수를 만들어서 전달
    - this.state
    - this.setState
    - setState 는 단순히 상태를 바꾸는 함수가 아니라 상태로 바꿔달라고 요청해주는 함수로 이해
    - 상태가 업데이트 되고 나서 어떤 작업을 하고 싶다면 setState 의 두번째 파라미터에 콜백함수를 넣어줌

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,41 +1,40 @@
-import React, { useReducer } from "react";
+import React, { Component } from "react";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      fixed: 1,
+    };
   }
-}
-
-function Counter() {
-  // const numberState = useState(0);
-  // const number = numberState[0];
-  // const setNumber = numberState[1];
-  // 배열 비구조화 할당을 통하여 각 원소를 추출
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    // console.log("+1");
-    // setNumber(number + 1);
-    dispatch({ type: "INCREMENT" });
-  };
-  const onDecrease = () => {
-    // console.log("-1");
-    // setNumber(number - 1);
-    dispatch({ type: "DECREMENT" });
+  handleIncrease = () => {
+    this.setState(
+      (state) => ({
+        counter: state.counter + 1,
+      }),
+      () => {
+        console.log(this.state.counter);
+      }
+    );
   };
 
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
+  handleDecrease = () => {
+    this.setState((state) => ({
+      counter: state.counter - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값: {this.state.fixed}</p>
+      </div>
+    );
+  }
 }
 
 export default Counter;
