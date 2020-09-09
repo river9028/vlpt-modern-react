8da2256f70cad12a3e6b843532f06edff9398c92
river <river9028@gmail.com>
2020-09-09 23:27:36 +0900

    :zap: 25. LifeCycle Method, 26. componentDidCatch 로 에러 잡아내기 / Sentry 연동 :zap:
    - LifeCycle Method: "생명주기 메서드", 클래스형 컴포넌트에서만 사용, useEffect 비슷
    - 마운트 constructor / getDerivedStateFromProps / render / componentDidMount
    - 업데이트 getDerivedStateFromProps / shouldComponentUpdate / render / getSnapshotBeforeUpdate / componentDidUpdate
    - 언마운트 componentWillUnmount
    
    - componentDidCatch 를 이용해 리액트 애플리케이션에서 발생하는 에러를 처리하는 방법
    - null checking
    - defaultProps(TypeScript / Flow 사용해 관리 권장)
    
    - Sentry: componentDidCatch 에서 error 와 info 값을 네트워크를 통하여 다른 곳으로 전달, 장기적 작업 프로젝트 적용 권장

---
+++ error-catch/src/User.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function User({ user }) {
+  // "null checking"
+  // if (!user) {
+  //   return null;
+  // }
+
+  return (
+    <div>
+      <div>
+        <b>ID</b>: {user.id}
+      </div>
+      <div>
+        <b>Username:</b> {user.username}
+      </div>
+    </div>
+  );
+}
+
+function Users({ users, onToggle }) {
+  if (!users) return null;
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id} onClick={() => onToggle(user.id)}>
+          {user.username}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+Users.defaultProps = {
+  onToggle: () => {
+    console.warn("onToggle is missing!");
+  },
+};
+
+export default User;
