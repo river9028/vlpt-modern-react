16a1c3a68fbc2979d4dede33f1843e94f2d1d84b
river <river9028@gmail.com>
2020-10-02 19:11:42 +0900

    :zap: 4. 리액트 라우터 부가기능 :zap: - history 객체를 통해 컴포넌트 내에 구현하는 메소드에서 라우터에 직접 접근 가능 - withRouter HoC 라우트 컴포넌트가 아닌곳에서 match / location / history 를 사용   자신의 부모 컴포넌트 기준의 match 값이 전달 - Switch: 여러 Route 들을 감싸서 그 중 규칙이 일치하는 라우트 단 하나만을 렌더링 - NavLink: 현재 경로와 Link 에서 사용하는 경로가 일치하는 경우 특정 스타일 혹은 클래스를 적용 가능 - Redirect: 페이지를 리디렉트 하는 컴포넌트 - Prompt: 이전에 사용했던 history.block 의 컴포넌트 버전 - Route Config: JSX 형태로 라우트를 선언하는 것이 아닌 Angular 나 Vue 처럼 배열/객체를 사용하여 라우트 정의하기 - Memory Router 실제로 주소는 존재하지는 않는 라우터. 리액트 네이티브나, 임베디드 웹앱에서 사용하면 유용하다.

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Link, Route } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -8,20 +9,32 @@ const Profiles = () => {
       <h3>유저 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/velopert">velopert</Link>
+          <NavLink
+            to="/profiles/velopert"
+            activeStyle={{ background: 'black', color: 'white' }}
+            // activeClassName={}
+          >
+            velopert
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">gildong</Link>
+          <NavLink
+            to="/profiles/gildong"
+            activeStyle={{ background: 'black', color: 'white' }}
+            // activeClassName={}
+          >
+            gildong
+          </NavLink>
         </li>
       </ul>
-
       <Route
         path="/profiles"
         exact
         // render: JSX 를 렌더링하는 것. 상위 영역에서 props 나 기타 값들을 필요하면 전달 가능
         render={() => <div>유저를 선택해주세요.</div>}
       />
-      <Route path="/profiles/:username" component={Profile} />
+      <Route path="/profiles/:username" component={Profile} />{' '}
+      <WithRouterSample />
     </div>
   );
 };
