7e157decd0158d1cfd36d084c10a6199b0f46528
river <river9028@gmail.com>
2020-10-02 18:11:18 +0900

    :zap: 2. 파라미터와 쿼리 :zap: - 페이지 주소를 정의 할 때, 우리는 유동적인 값을 전달   파라미터: /profiles/velopert(주로 특정 id 나 이름을 가지고 조회를 할 때 사용)   쿼리: /about?details=true (주로 키워드를 검색하거나, 요청을 할 때 필요한 옵션을 전달에 사용) - 파라미터를 받아올 땐 match 객체 안에 들어있는 params 값을 참조 - 쿼리는 location 객체에 있는 search 문자열 형태 값을 참조. 객체 형태로 변환해주는 qs 라이브러리 이용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
 
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
