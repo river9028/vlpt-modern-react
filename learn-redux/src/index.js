03bbdccf1fb41992dcc8d3037cdb99202feb293f
river <river9028@gmail.com>
2020-10-04 18:09:46 +0900

    :zap: 4. 리덕스 모듈 만들기 :zap:
    - 리덕스 모듈: 액션 타입, 액션 생성함수, 리듀서 항목들이 모두 들어있는 자바스크립트 파일. 분리가 되어있으면 개발 불편 리듀서와 액션 관련 코드들을 하나의 파일에 몰아서 작성하는걸 Ducks 패턴이라고 한다
    - 루트 리듀서: 합쳐진 리듀서. 한 프로젝트에 여러개의 리듀서가 있을때는 combineReducers라는 함수를 이용해 이를 한 리듀서로 합쳐서 사용
    
    :zap: 5. 카운터 구현하기 :zap:
    
    - 컨테이너 컴포넌트: 리덕스 스토어의 상태를 조회하거나, 액션을 디스패치 할 수 있는 컴포넌트
    - 프리젠테이셔널 컴포넌트와 컨테이너 컴포넌트를 분리해서 작업
    
    :zap: 6. 리덕스 개발자도구 적용하기 :zap:
    
    -리덕스 개발자 도구: 현재 스토어의 상태를 개발자 도구에서 조회 할 수 있고 지금까지 어떤 액션들이 디스패치 되었는지, 그리고 액션에 따라 상태가 어떻게 변화했는지 확인 가능
    - redux-devtools-extension 설치
    
    :zap: 7. 할 일 목록 구현하기 :zap:
    
    :zap: 8. useSelector 최적화 :zap:
    - useSelector를 사용해서 리덕스 스토어의 상태를 조회 할 땐 만약 상태가 바뀌지 않았으면 리렌더링하지 않음
    - 최적화 1. useSelector 를 여러번 사용하는 것
    - 최적화 2. react-redux의 shallowEqual 함수를 useSelector의 두번째 인자로 전달해주는 것
    - useSelector 의 두번째 파라미터: equalityFn, 이전 값과 다음 값을 비교하여 true가 나오면 리렌더링을 하지 않고 false가 나오면 리렌더링

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import './exercise';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules/';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
+
+// console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
