1b4dc60fe5124f1e709c7aa40ee75d718c52f8f9
river <river9028@gmail.com>
2020-10-08 02:45:40 +0900

    :zap: 6. API 재로딩 문제 해결하기 :zap:
    - 포스트 목록 재로딩 문제 해결하기
      1. 만약 데이터가 이미 존재한다면 요청을 하지 않도록 하는 방법
      2. 로딩을 새로하긴 하는데, 로딩중... 을 띄우지 않는 것, 사용자에게 좋은 경험을 제공, 최신 데이터를 보여줄 수 있음
    - 어떤 파라미터가 주어졌냐에 따라 다른 결과물이 있는 포스트 조회시 재로딩 문제 해결하기
      1. 컴포넌트가 언마운트될때 포스트 내용을 비우도록 하는 것 => 이미 읽었던 포스트를 불러오려고 할 경우에도 새로 요청을 한다는 것이 단점
      2. posts 모듈에서 포스트 ID에 해당하는 정보를 하나씩 저장해 조회할 수 있도록 관리하는 상태로 구조를 바꿔 data 존재시 로딩중...을 보여주지 않도록 함.

--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,7 +1,9 @@
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
 import {
   createPromiseThunk,
+  createPromiseThunkById,
   handleAsyncActions,
+  handleAsyncActionsById,
   reducerUtils,
 } from '../lib/asyncUtils';
 
@@ -16,6 +18,9 @@ const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
+
+// 포스트 비우기
+const CLEAR_POST = 'CLEAR_POST';
 
 /* 
  thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
@@ -45,7 +50,10 @@ export const getPost = (id) => async (dispatch) => {
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+// export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
+// export const clearPost = () => ({ type: CLEAR_POST });
 
 /* 
 const initialState = {
@@ -65,7 +73,8 @@ const initialState = {
 // initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  // post: reducerUtils.initial(),
+  post: {},
 };
 
 /* 
@@ -174,11 +183,17 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
+      // return handleAsyncActions(GET_POST, 'post', true)(state, action);
+      return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+    // case CLEAR_POST:
+    //   return {
+    //     ...state,
+    //     post: reducerUtils.initial(),
+    //   };
     default:
       return state;
   }
